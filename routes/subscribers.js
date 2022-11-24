91bfe41c2a5da91fbbe1b01c431ff44cb9efec76
Pranay Mangal <mangal.pranay@gmail.com>
2022-11-25 03:55:00 +0530

    rest api complete

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,25 +1,73 @@
 const express = require('express')
 const router = express.Router()
-
+const Subscriber = require('../models/subscriber')
 //getting all subscribe
-router.get('/',(req,res)=>{
-res.send('Hello world')
+router.get('/',async(req,res)=>{
+try {
+   const subscribers =  await Subscriber.find()
+   res.json(subscribers)
+} catch (error) {
+    res.status(500).json({message: error.message})
+}
+   
 })
 //geting one
-router.get('/:id',(req,res)=>{
-req.params.id
+router.get('/:id',getSubscriber,(req,res)=>{
+res.json(res.subscriber)
 
 })
 //creating one
-router.post('/',(req,res)=>{
+router.post('/',async(req,res)=>{
+const subscriber = new Subscriber({
+  name : req.body.name,
+  subscriberToChannel : req.body.subscriberToChannel  
+})
 
+try {
+   const newSubscriber = await subscriber.save()
+   res.status(201).json(newSubscriber) 
+} catch (error) {
+    res.status(400).json({message : error.message})
+}
 })
 //updating one
-router.patch('/:id',(req,res)=>{
 
+router.patch('/:id',getSubscriber,async(req,res)=>{
+if(req.body.name!= null){
+    res.subscriber.name = req.body.name
+}
+if(req.body.subscriberToChannel!= null){
+    res.subscriber.subscriberToChannel = req.body.subscriberToChannel
+}
+
+try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+} catch (error) {
+    res.status(400).json({message : error.message})
+}
 })
 //deleting one
-router.delete('/:id',(req,res)=>{
-
+router.delete('/:id',getSubscriber,async(req,res)=>{
+try {
+    await res.subscriber.remove()
+    res.json({message: 'deleted subscriber'})
+} catch (error) {
+   res.status(500).json({message: error.message}) 
+}
 })
+
+async function getSubscriber(req,res,next){
+let subscriber
+try {
+    subscriber = await Subscriber.findById(req.params.id)
+   if(!subscriber){
+    return res.status(404).json({message: 'cannont find subscriber'})
+   }
+} catch (error) {
+  return  res.status(500).json({message : error.message})
+}
+res.subscriber = subscriber 
+next()
+}
 module.exports = router
